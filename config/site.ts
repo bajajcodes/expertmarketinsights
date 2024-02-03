b812f5fb6d3ce1d020890d6a886bdb6b1e2888f3
shubham bajaj <shmbajaj32@gmail.com>
2024-02-03 14:48:52 +0530

    feat: added assets

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: 'Expert Market Insights',
-  url: 'https://emi.com',
+  url: 'https://expertmarketinsights.com',
   description:
     'Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.',
   links: {
---
+++ data/home-page.ts
@@ -0,0 +1,14 @@
+export const images = {
+  header: {
+    src: 'https://res.cloudinary.com/dvi5qujxs/image/upload/v1706871715/expertmarketinsights/EMIHEADER_of7zyp.jpg',
+    alt: 'Header',
+  },
+  introduction1: {
+    src: 'https://res.cloudinary.com/dvi5qujxs/image/upload/v1706870075/expertmarketinsights/introduction-1_j9ey5d.jpg',
+    alt: 'Introduction',
+  },
+  introduction2: {
+    src: 'https://res.cloudinary.com/dvi5qujxs/image/upload/v1706870077/expertmarketinsights/introduction-2_vo34u5.jpg',
+    alt: 'Introduction',
+  },
+};
