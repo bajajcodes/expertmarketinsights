c1dc69ec538d8124926604a33cd75940798a8efc
shubham bajaj <shmbajaj32@gmail.com>
2024-02-09 20:15:38 +0530

    feat: moved frontend code inside frontend dir

--- types/schema.ts
+++
@@ -1,5 +0,0 @@
-import { newsLetterSchema, sendLeadSchema } from '@/utils/schema';
-import { z } from 'zod';
-
-export type LeadGenerateType = z.infer<typeof sendLeadSchema>;
-export type NewsLetterType = z.infer<typeof newsLetterSchema>;
