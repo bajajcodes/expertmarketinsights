8794d629cb5c2a6c6ab5524a1f8757a30d56c9ed
shubham bajaj <shmbajaj32@gmail.com>
2024-02-24 16:01:25 +0530

    feat:added 404 page

---
+++ frontend/app/not-found.tsx
@@ -0,0 +1,11 @@
+import { PageHeader, PageHeaderHeading } from "@/components/page-header";
+
+export default function NotFound() {
+  return (
+    <section className="container">
+      <PageHeader>
+        <PageHeaderHeading>404: Not Found )</PageHeaderHeading>
+      </PageHeader>
+    </section>
+  );
+}
