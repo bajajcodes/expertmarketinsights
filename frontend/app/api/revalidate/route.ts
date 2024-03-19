a44d6a40a52793c807f901c10fb5d67d3483407f
shubham bajaj <shmbajaj32@gmail.com>
2024-03-19 12:42:07 +0530

    reafctor: revalidate path for reports page

--- frontend/app/api/revalidate/route.ts
+++ frontend/app/api/revalidate/route.ts
@@ -1,3 +1,5 @@
+import { getCategoryByReportId } from "@/app/actions";
+import { $TsFixMe } from "@/app/types";
 import { API_CACHE_TAGS } from "@/types/api";
 import { getSlug } from "@/utils/slugs";
 import { revalidatePath, revalidateTag } from "next/cache";
@@ -8,35 +10,51 @@ export async function GET() {
 }
 
 export async function POST(request: NextRequest) {
-  const payload = await request.json();
+  try {
+    const payload = await request.json();
 
-  if (payload?.model === "category") {
-    const name = payload.entry.name;
-    const id = payload.entry.id;
-    const slug = getSlug(name, id);
+    if (payload?.model === "category") {
+      const name = payload.entry.name;
+      const id = payload.entry.id;
+      const slug = getSlug(name, id);
 
-    revalidateTag(API_CACHE_TAGS.CATEGORY_SLUGS);
-    revalidateTag(API_CACHE_TAGS.CATEGORIES);
-    revalidatePath("/categories", "page");
-    revalidatePath(`/reports/${slug}`, "page");
+      revalidateTag(API_CACHE_TAGS.CATEGORY_SLUGS);
+      revalidateTag(API_CACHE_TAGS.CATEGORIES);
+      revalidatePath("/categories", "page");
+      revalidatePath(`/reports/${slug}`, "page");
+    }
+
+    if (payload?.model === "report") {
+      const id = payload.entry.id;
+      const name = payload.entry.reportTitle;
+      const reportSlug = getSlug(name, id);
+      const category = await getCategoryByReportId(id);
+
+      revalidateTag(API_CACHE_TAGS.CATEGORY_SLUGS);
+      revalidateTag(API_CACHE_TAGS.CATEGORIES);
+
+      if (!category) return;
+
+      const categorySlug = getSlug(category.title, category.id);
+
+      revalidatePath(`/reports/${categorySlug}/${reportSlug}`, "page");
+      revalidatePath(`/reports/${categorySlug}`, "page");
+      revalidatePath("/reports", "page");
+    }
+
+    return NextResponse.json({ message: "Revalidate Done", revalidate: true });
+  } catch (err: $TsFixMe) {
+    return NextResponse.json(
+      {
+        message: `Revalidate Failed: ${
+          err?.message || "Failed To Revalidate, Something went Wrong!!"
+        }`,
+        revalidate: false,
+      },
+      {
+        status: 400,
+        statusText: "Revalidate Path Failed.",
+      }
+    );
   }
-
-  if (payload?.model === "report") {
-    //TODO: call api to get category slug for a report
-    // const id = payload.entry.id;
-    // const name = payload.entry.reportTitle;
-    // const reportSlug = getSlug(name, id);
-
-    revalidateTag(API_CACHE_TAGS.CATEGORY_SLUGS);
-    revalidateTag(API_CACHE_TAGS.CATEGORIES);
-    revalidateTag(API_CACHE_TAGS.CATEGORY_REPORTS);
-    revalidateTag(API_CACHE_TAGS.REPORT_BY_ID);
-    revalidateTag(API_CACHE_TAGS.REPORTS);
-
-    revalidatePath("/reports", "page");
-  }
-
-  revalidatePath("/", "layout");
-  revalidatePath("/", "page");
-  return NextResponse.json({ message: "Revalidate Done", revalidate: true });
 }
